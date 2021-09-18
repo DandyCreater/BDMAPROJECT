e9d5ff66c0280894943175404775e9511ae1276e
KstriaBatang <dandythirst22@gmail.com>
2021-09-18 22:17:53 +0700

    Progress Week 3

--- lib/scene/dashboard.dart
+++ lib/scene/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:bdma/common/helper.dart';
 import 'package:bdma/connectivity/connectivity_provider.dart';
+import 'package:bdma/scene/submenu/dtrack.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
@@ -71,78 +72,75 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            height: 200,
-            width: SizeConfig.ScreenWidth,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Color(0xFFF0730F),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 70,
-                        width: 70,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                              image: AssetImage("assets/img/Marco2019.png"),
-                              fit: BoxFit.cover),
-                          borderRadius: BorderRadius.circular(100),
-                          // color: Colors.amber,
+          SingleChildScrollView(
+            child: Container(
+              height: 200,
+              width: SizeConfig.ScreenWidth,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xFFF0730F),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 70,
+                          width: 70,
+                          decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image: AssetImage("assets/img/Marco2019.png"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(100),
+                            // color: Colors.amber,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 20,
-                        height: 160,
-                      ),
-                      Container(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              child: Text(
-                                fullname,
-                                style: TextStyle(
-                                    fontSize: 25,
-                                    color: Colors.white,
-                                    fontFamily: 'Raleway',
-                                    fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 12,
-                            ),
-                            Container(
-                              padding: EdgeInsets.only(right: 62),
-                              child: Text(
-                                username,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.grey[100]),
-                              ),
-                            )
-                          ],
+                        SizedBox(
+                          width: 20,
+                          height: 160,
                         ),
-                      ),
-                      SizedBox(
-                        width: 100,
-                      ),
-                      pageUI(),
-                      // Container(
-                      //   height: 30,
-                      //   width: 30,
-                      //   color: Colors.green,
-                      // )
-                    ],
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                child: Text(
+                                  fullname,
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      color: Colors.white,
+                                      fontFamily: 'Raleway',
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 12,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(right: 62),
+                                child: Text(
+                                  username,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.grey[100]),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 100,
+                        ),
+                        pageUI(),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Container(
@@ -198,7 +196,10 @@ class _DashboardState extends State<Dashboard> {
                   height: 70,
                   width: SizeConfig.ScreenWidth,
                   child: FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Dtrack()));
+                    },
                     child: Image.asset("assets/img/DTRACK.png"),
                   ),
                 ),
